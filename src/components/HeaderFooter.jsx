481a04ce73fae42f38ec0adcdb677724ddd199ee
hillrg22 <hillrg22@gmail.com>
2018-10-12 10:54:52 -0600

    began structuring HeaderFooter component

--- src/components/HeaderFooter.jsx
+++ src/components/HeaderFooter.jsx
@@ -1,7 +1,9 @@
 //stateless component
 
 const HeaderFooter = () => {
-
+  return (<nav className="navbar navbar-dark bg-primary">
+      <a className="navbar-brand" href="#">Memory Grid</a>
+    </nav>
 
 }
 
