32c4a945183c96ec527881a3b2e7281ab785de7b
charityvanb <charityvanb@gmail.com>
2018-10-12 11:48:03 -0600

    made footer seperate

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const footer = () => {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">copyright CCR 2018</a>
+        </nav>
+    )
+}
+
+export default footer;
