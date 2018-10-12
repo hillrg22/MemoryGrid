83af6aec65e8a9d6b9184c383428fe72e09a1bd9
Conrad Wright <conrad@Conrads-MacBook-Pro.local>
2018-10-12 12:06:05 -0600

    basic grid, one square

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import GameBoard from './components/GameBoard'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <GameBoard />
+
       </div>
     );
   }
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,13 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import '../style.css'
 
-class GameBoard extends Component {
+export default class Example extends React.Component {
   render() {
     return (
       <div>
-        
+      <Container className="my-5 mx-4" id="main-container">
+        <Row className="grid-row">
+          <Col xs="3">
+              <div className="numBox">
+                  <span className="text">1</span>
+              </div>
+          </Col>
+          <Col xs="3">
+          <span>Box Span</span>
+          </Col>
+          <Col xs="3">
+          <span>Box Span</span>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs="3">Box</Col>
+          <Col xs="3">Box </Col>
+          <Col xs="3">Box</Col>
+        </Row>
+        <Row>
+          <Col xs="3">Box</Col>
+          <Col xs="3">Box </Col>
+          <Col xs="3">Box</Col>
+        </Row>
+        </Container>
       </div>
     );
   }
 }
 
-export default GameBoard;
